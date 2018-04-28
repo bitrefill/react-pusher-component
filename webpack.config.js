a991ea5d3a39a08511942a94524dda4296df3a17
fred wright <fredwright0@gmail.com>
2018-04-28 22:48:21 +0700

    🎉 update api

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, './dist'),
     filename: 'index.js',
-    library: 'react-pusher',
+    library: 'react-pusher-component',
     libraryTarget: 'umd',
   },
   resolve: {
